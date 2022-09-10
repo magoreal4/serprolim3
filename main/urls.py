29e9006d9dd5baa73b214a66d152e12546a27282
magoreal <magoreal4@gmail.com>
2022-09-09 20:25:08 -0400

    first commit

---
+++ main/urls.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from django.urls import include, path
+from django.contrib import admin
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+from search import views as search_views
+
+from wagtail_favicon.urls import urls as favicon_urls
+
+from wagtail.contrib.sitemaps.views import sitemap
+
+urlpatterns = [
+    path("django-admin/", admin.site.urls),
+    path("admin/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("search/", search_views.search, name="search"),
+    path('sitemap.xml', sitemap),
+]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += path("__reload__/", include("django_browser_reload.urls")),
+
+urlpatterns = urlpatterns + [
+    path("", include(wagtail_urls)),
+    path("", include(favicon_urls)),
+]
